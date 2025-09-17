0b32ddaeddf2eaf7d33987d5ba7796afc0c4b6ca
TistaMilani <francesco.rocchiccioli.02@gmail.com>
2025-09-17 23:57:15 +0200

    Test favicon

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  // Set favicon dynamically
-  useEffect(() => {
-    const setFavicon = (href: string) => {
-      let link = document.querySelector("link[rel*='icon']") as HTMLLinkElement
-      if (!link) {
-        link = document.createElement('link')
-        link.type = 'image/x-icon'
-        link.rel = 'shortcut icon'
-        document.getElementsByTagName('head')[0].appendChild(link)
-      }
-      link.href = href
-    }
-
-    // Set the main favicon
-    setFavicon('./favicon.ico')
-  }, [])
+  // Favicon is now set in index.html for better GitHub Pages compatibility
 
   return (
     <>
